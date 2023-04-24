79c00121fd806edc53fb60bb5aa96c0d2c0fadab
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2023-04-24 15:34:18 -0700

    Added Input System into Player Movement

--- GDIM 61 2D Project/Assets/Scripts/PlayerMovement.cs
+++ GDIM 61 2D Project/Assets/Scripts/PlayerMovement.cs
@@ -5,40 +5,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //[SerializeField]
-    //protected InputAction horizontalMovement;//A set of bindings for moving left to right
-
-    //[SerializeField]
-    //protected Rigidbody2D rb;//The object's rigidbody to apply movement force.
-
-    //[SerializeField]
-    //protected int moveForce;//A force to move the object.
-
-    //protected Vector2 applyToMove;//The movement to apply 
-
-    //protected void OnEnable()
-    //{
-    //    horizontalMovement.Enable();
-    //}
-
-    //protected void OnDisable()
-    //{
-    //    horizontalMovement.Disable(); 
-    //}
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    Movement();
-    //}
-
-    //protected virtual void Movement()
-    //{
-    //    float horizontalInput = horizontalMovement.ReadValue<float>();
-    //    applyToMove = new Vector2(horizontalInput * moveForce * Time.deltaTime, 0);
-    //    transform.Translate(applyToMove, Space.World); 
-        
-
-
-    //}
+    
 
 }
