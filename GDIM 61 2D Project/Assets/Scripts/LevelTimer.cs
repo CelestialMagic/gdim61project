6eb184e76900fdfe053d43e064fe02747a2d32c3
Alex-Bennett-UCI <atbennet@uci.edu>
2023-04-26 19:31:04 -0700

    LevelTimer script "beta"

--- GDIM 61 2D Project/Assets/Scripts/LevelTimer.cs
+++ GDIM 61 2D Project/Assets/Scripts/LevelTimer.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //The global timer for the scene
+    [SerializeField]
+    private float levelTime;
+
+    //The player in the scene
+    [SerializeField]
+    private PlayerMovement player;
+
+    //The spawner in the scene
+    [SerializeField]
+    private FallingObstacleSpawner spawner;
+
+    //Text display of level timer
+    [SerializeField]
+    private Text timeDisplay;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Countdown();
+    }
+
+    private void Countdown()
+    {
+        if ((levelTime - Time.deltaTime) <= 0)
+        {
+
+        }
+        else
+        {
+            levelTime -= Time.deltaTime;
+            timeDisplay.text = $"Time: {(int) levelTime}";
+        }
     }
 }
