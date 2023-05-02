7e1b54e9f93f5532cc3c1af264a22a5e80f3bcd2
Alex-Bennett-UCI <atbennet@uci.edu>
2023-05-01 21:34:46 -0700

    Dig Site Spawner prototype

--- GDIM 61 2D Project/Assets/Scripts/Digging Game Scripts/DigSiteSpawner.cs
+++ GDIM 61 2D Project/Assets/Scripts/Digging Game Scripts/DigSiteSpawner.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class DigSiteSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private List<Transform> digSites;
+
+    [SerializeField]
+    private List<GameObject> buriedTreasure;
+   
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,10 @@ public class DigSiteSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Transform randomSite = digSites[(int)(Random.Range(0, digSites.Count))];
+        gameObject.transform.position = new Vector3 (randomSite.position.x, randomSite.position.y, 0);
+        GameObject randomTreasure = buriedTreasure[(int)(Random.Range(0, buriedTreasure.Count))];
+        randomTreasure.transform.position = gameObject.transform.position;
+        Instantiate(randomTreasure);
     }
 }
